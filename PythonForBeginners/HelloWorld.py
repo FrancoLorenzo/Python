cce0c8f85541e052da5f423b44cce1ce930fd90c
FrancoLorenzo <franco.lorenzo13@outlook.com>
2021-09-18 23:44:56 -0600

    First commit

---
+++ PythonForBeginners/HelloWorld.py
@@ -0,0 +1,19 @@
+print('Hello World')
+
+# Print blank line
+print()
+print('Did you see that blank line?')
+print('Blank line \nin the middle of string.')
+
+# Input command to request user input
+name = input ('Please enter your name: ')
+print('Hi ' + name + '!')
+
+# Debugging with print
+print('Adding numbers')
+x = 42 + 206
+print('Performing division')
+y = x / 0
+
+# The following print will not run due to a zero division error
+print('Math complete')
---
+++ PythonForBeginners/Numbers.py
@@ -0,0 +1,28 @@
+# pi = 3.14159
+# print(pi)
+
+# Symbol  Operation
+# +       Addition
+# -       Substraction
+# *       Multiplication
+# /       Division
+# **      Exponent
+
+first_number = 6
+second_number = 2
+print(first_number + second_number)
+print(first_number ** second_number)
+
+# Converting numbers into strings
+days_in_feb = 28
+print(str(days_in_feb) + ' days in February')
+
+# Converting strings into integers
+first_number = input('Enter first number: ')
+second_number = input('Enter second number: ')
+print(int(first_number) + int(second_number))
+
+# Converting strings into floats
+first_number = input('Enter first number with decimals: ')
+second_number = input('Enter second number with decimals: ')
+print(float(first_number) + float(second_number))
